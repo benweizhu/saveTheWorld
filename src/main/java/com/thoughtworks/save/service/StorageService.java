e2e1222a90e58a01d6e50e87af5388d350c3eb8a
benweizhu <benweizhu@gmail.com>
2016-09-17 20:55:51 +0800

    first happy test case

---
+++ src/main/java/com/thoughtworks/save/service/StorageService.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.save.service;
+
+public class StorageService {
+    public String getSnapShot(String historyData, String id) {
+        return "cat1 15 12\ncat2 2 3";
+    }
+}
